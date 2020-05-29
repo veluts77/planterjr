df29a09ce8150c7640e589c12c4283211a98b56f
veluts77 <veluts77@gmail.com>
2020-05-29 23:41:30 +0000

    Cleaned unused code lines

--- main/src/main/ruby/main.rb
+++ main/src/main/ruby/main.rb
@@ -1,30 +1,10 @@
 #!/usr/bin/env jruby
 # frozen_string_literal: true
 
-puts 'Hello from core Ruby!!!'
-
-###
-
-class MyApp
-  require './java-obj.jar'
-  include_package 'objects'
-
-  def run
-    puts 'Working with java object from imported package'
-    obj = JavaObject.new
-    obj.name = '"Ruby rocks"'
-    res = obj.greetingsMessage
-    puts res
-  end
-end
-
-MyApp.new.run
-
-###
-
 require_relative 'ui/mainframe.rb'
 MainFrame.new
 
 require_relative 'planter/test_renderer.rb'
 require './uml-renderer-all.jar'
+require './java-obj.jar'
 TestRenderer.new.run
